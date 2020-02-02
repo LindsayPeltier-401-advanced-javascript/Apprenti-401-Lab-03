016981864e52ae5bedad2ad7af41482b85fbb6e4
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-02 12:38:41 -0800

    refactor

--- edit-file.js
+++ edit-file.js
@@ -57,34 +57,3 @@ async function writeFile() {
 writeFile();
 
 module.exports = { readFile, writeFile };
-
-// 'use strict';
-
-// const fs = require('fs');
-// const validator = require('./validator/validator');
-
-// const read = (file, callback) => {
-//   fs.readFile(file, (error, data) => {
-//     if (error) {
-//       callback(error);
-//     } else {
-//       callback(undefined, JSON.parse(data.toString()));
-//     }
-//   });
-// };
-
-// const save = (data, fileName, rules, callback) => {
-//   if (validator.isValid(rules, data)) {
-//     const buffer = Buffer.from(JSON.stringify(data));
-
-//     fs.writeFile(fileName, buffer, error => {
-//       if (error) {
-//         callback(error);
-//       } else {
-//         callback(undefined);
-//       }
-//     });
-//   } else { console.log('Is Valid'); }
-// };
-
-// module.exports = { read, save };
